65ecb1cb87f4c5ea089c3c431317f0a634eb5846
Jacek Pospychala <jacek.pospychala@gmail.com>
2013-10-28 19:31:56 +0100

    finish remaining APIs

--- src/rest_app.js
+++ src/rest_app.js
@@ -20,41 +20,66 @@ json = function(res, object) {
     res.send(JSON.stringify(object));
 }
 
-not_implemented = function(req, res){
-    json(res, {'error':'not implemented'});
-}
-
 var market = new model.Market();
 
 get = function(fn) {
     return function(req, res) {
         json(res, fn());
     }
+};
+
+to_products = function(obj) {
+    var prods = [];
+    for (var name in obj) {
+        var price = obj[name];
+        prods.push(new model.Product(name, price, 999999));
+    }
+    return prods;
+};
+
+to_broker = function(obj) {
+    return new model.Broker(obj.name, obj.secret, obj.money);
 }
 
-get_rates = function(req, res){
-    json(res, market.products.list());
-}
-
-get_brokers = function(req, res){
-    json(res, market.brokers.list());
+to_transaction = function(obj) {
+    return {
+        'broker_name': obj.broker_name,
+        'product_name': obj.product_name,
+        'amount': obj.amount,
+        'unit_price_min': obj.unit_price_min,
+        'unit_price_max': obj.unit_price_max,
+    };
 }
 
 set_rates = function(req, res) {
-    var obj = req.body;
-    for (var name in obj) {
-        var price = obj[name];
-        market.products.add(new model.Product(name, price, 999999));
-    }
+    var prods = to_products(req.body);
+    market.products.addAll(prods);
     json(res, market.products.list());
-}
+};
 
+set_broker = function(req, res) {
+    var broker = to_broker(req.body);
+    market.brokers.add(broker);
+    json(res, market.brokers.get(broker.name));
+};
 
-app.get('/buy', not_implemented);
-app.get('/sell', not_implemented);
-app.get('/rates', get_rates);
+buy = function(req, res) {
+    var tr = to_transaction(req.body);
+    market.buy(tr.broker_name, tr.product_name, tr.amount, tr.unit_price_min, tr.unit_price_max);
+    json(res, market.brokers.get(tr.broker_name));
+};
+
+sell = function(req, res) {
+    var tr = to_transaction(req.body);
+    market.sell(tr.broker_name, tr.product_name, tr.amount, tr.unit_price_min, tr.unit_price_max);
+    json(res, market.brokers.get(tr.broker_name));
+};
+
+app.post('/buy', buy);
+app.post('/sell', sell);
+app.get('/rates', get(function() {return market.products.list(); }));
 app.post('/rates', set_rates);
-app.get('/brokers', get_brokers);
-app.post('/broker', not_implemented);
+app.get('/brokers', get(function() {return market.brokers.list(); }));
+app.post('/broker', set_broker);
 
 app.listen(3000);
