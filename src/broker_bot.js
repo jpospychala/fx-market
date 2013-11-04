13174be2e58e72f11dbe44426d5ee3c43d94223f
Jacek Pospychala <jacek.pospychala@gmail.com>
2013-11-04 18:34:16 +0100

    more elaborate analysis

--- src/broker_bot.js
+++ src/broker_bot.js
@@ -15,52 +15,59 @@ store_broker = function(data) {
 
 var analyze_rates = function(data) {
     if (model.rates) {
-        var max_delta = -1;
-        var max_delta_currency = undefined;
+        var tmp = [];
         for (var currency in data) {
             if (model.rates[currency]) {
                 var old_price = model.rates[currency].price;
                 var new_price = data[currency].price;
                 var delta = new_price - old_price;
+                var old_delta = model.rates[currency].delta || 0;
                 data[currency].delta = delta;
-                if (delta > max_delta) {
-                    max_delta = delta;
-                    max_delta_currency = currency;
-                }
+                var score = (old_delta > 0 ? 1 : 0) + delta;
+                tmp.push({'delta':delta, 'delta2': old_delta, 'name':currency, 'score':score});
             }
         }
-        model.to_buy = max_delta_currency;
+        model.sorted = tmp.sort(function(a, b) {return b.score-a.score });
+        model.to_buy = model.sorted[0].name;
     }
     model.rates = data;
     model.rates.timestamp = new Date();
 };
 
-broker_loop = function() {
-    broker_api.get_rates().then(analyze_rates, error);
-    
-    if (!model.rates) {
-        return;
-    }
-    
+buy_and_sell = function() {
     console.log('model ', model);
     
     if (model.to_buy === undefined) {
         return;
     }
     
-    var unit_price = model.rates[model.to_buy].price;
-    var upmin = unit_price - 0.1;
-    var upmax = unit_price + 0.1;
-    var amount = Math.floor(model.broker.money / unit_price);
-    if (amount > 0) {
-        broker_api.buy(broker_name, broker_secret, model.to_buy, amount, upmin, upmax).then(store_broker, error);
-    }
-    
-    for (var c in model.broker.wallet.items) {
-        if (c !== model.to_buy) {
-            broker_api.sell(c, model.broker.wallet.items[c].amount, 0, 1000).then(store_broker, error);
+    if (Math.random() > 0.6) {
+        var unit_price = model.rates[model.to_buy].price;
+        var delta = model.rates[model.to_buy].delta;
+        var upmin = unit_price - delta;
+        var upmax = unit_price + delta;
+        var amount = Math.floor(0.25* model.broker.money / unit_price);
+        if (amount > 0) {
+            broker_api.buy(broker_name, broker_secret, model.to_buy, amount, upmin, upmax).then(store_broker, error);
+        }
+    } else if (Math.random() > 0.5) {
+        for (var c in model.broker.wallet.items) {
+            if (c !== model.to_buy) {
+                var amount = model.broker.wallet.items[c].amount;
+                var price = model.rates[c].price;
+                var delta = 100;
+                broker_api.sell(broker_name, broker_secret, c, amount, price - delta, price + delta).then(store_broker, error);
+            }
         }
     }
+}
+
+broker_loop = function() {
+    broker_api.get_rates().then(function(data) {
+        analyze_rates(data);
+        buy_and_sell();
+    }, error);
+    
 }
 
 console.log('broker bot');
